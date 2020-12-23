ea893efdae482446c49627c485a949c6aedcf7b8
Themi <Tcm4760@gmail.com>
2020-12-23 00:32:37 -0600

    -added note to readme.md about physics implementation -started physics function

--- readme.md
+++ readme.md
@@ -4,4 +4,8 @@ This will soon be a drone flight simulator that will run completely in your brow
 
 The thing I am looking most forward to in this project is programming in the physics. Just thinking about how I'm going to implement the physics excites me. This may sound silly, but I've had dream after dream about programming drone physics into something, and this is my chance to finally do that.
 
+A note about my implementation of physics:
+
+I know babylon already has a physics engine I can use and that would be much much easier to do, but that takes away the fun part. I want to write it on my own even if it's more difficult to do and it's worse. The math excites me even though I know it's relatively basic
+
 ![Tracker image](https://imgtraker.herokuapp.com/img/KbtwQsiE.jpeg)
--- src/app/app.js
+++ src/app/app.js
@@ -10,8 +10,8 @@ export function startGame() {
     scene.clearColor = new BABYLON.Color3.White();
     scene.createDefaultEnvironment();
 
-    const camera = new BABYLON.FreeCamera("main_cam", new BABYLON.Vector3(0, 10, 500), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
+    const playerCamera = new BABYLON.FreeCamera("main_cam", new BABYLON.Vector3(0, 10, 500), scene);
+    playerCamera.setTarget(BABYLON.Vector3.Zero());
 
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
@@ -23,7 +23,12 @@ export function startGame() {
 
     //start render loop
     engine.runRenderLoop(function () {
+        runPhysicsStep(engine.getDeltaTime(), playerCamera);
         scene.render();
     });
+
+}
+
+function runPhysicsStep(frameTimeDelta, camera) {
 
 }
