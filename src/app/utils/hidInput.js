5a93edd359fa5ec443a7331cff7b89d448006590
Themi <Tcm4760@gmail.com>
2020-12-28 03:25:13 -0600

    -Basic idea of physics math down, needs fixing -hidInput throttle range changed to 0-1 instead of -1-1

--- src/app/utils/hidInput.js
+++ src/app/utils/hidInput.js
@@ -4,7 +4,8 @@ export function getHidState() {
     let gamepad = navigator.getGamepads()[0];
     if (gamepad !== undefined && gamepad !== null) {
         return {
-            throttle: gamepad.axes[0],
+            //throttle is on a range of 0-1 instead of -1-1. A negative throttle value does not make sense.
+            throttle: (gamepad.axes[0]+1)/2,
             yaw: gamepad.axes[3],
             pitch: gamepad.axes[2],
             roll: gamepad.axes[1],
--- src/app/utils/physics.js
+++ src/app/utils/physics.js
@@ -3,24 +3,53 @@ export class Physics {
         this.camera = camera;
         this.getHidState = getHidState;
         this.forcesScale = forcesScale;
+        this.motorPower = 4;
+        //max rotations per second in radians
+        this.rates = 2.7;
+        //added camera angle in degrees
         this.camAngle = 14;
+        this.camAngleRad = this.camAngle * (Math.PI/180);
         this.pitch = new Angle(0);
         this.roll = new Angle(0);
         this.yaw = new Angle(0);
     }
 
     runPhysicsStep(frameTimeDelta) {
-        //let hidState = this.getHidState();
-        this.pitch.value += Math.PI/(60*4);
+        let hidState = this.getHidState();
 
-        //apply to the actual camera
-        this.camera.rotation.x = this.pitch.value;
+        //end function if gamepad is not detected
+        if (!hidState.online)
+            return
+
+        //translate throttle stick into force (feet per second)
+        let systemForce = hidState.throttle * (9.8*this.motorPower);
+        //translate pitch and roll stick into rotations
+        this.pitch.value += (this.rates * hidState.pitch)/1000 * frameTimeDelta;
+        this.roll.value += (this.rates * hidState.roll)/1000 * frameTimeDelta;
+        this.yaw.value += (this.rates * hidState.yaw)/1000 * frameTimeDelta;
+        //find amount of force in each axis
+        let angleFromPlane = Math.atan(Math.sqrt(Math.tan(this.pitch.value)**2 + Math.tan(this.roll.value)**2));
+        let zForce = (((1/(Math.PI/4))*Math.abs(angleFromPlane-(Math.PI/2))-1)*systemForce);
+        let xyForce = systemForce - Math.abs(zForce);
+        let xForce = Math.abs(this.pitch.value)/(this.roll.value+this.pitch.value)*xyForce*Math.sign(this.pitch.value);
+        let yForce = (xyForce - Math.abs(xForce)) * Math.sign(this.pitch.value);
+        //adjust forces for yaw
+        //uc for unit circle
+        let ucAngle = Math.atan(xForce/yForce);
+        let ucHypotenuse = xForce/Math.sin(ucAngle);
+        let adjXForce = ucHypotenuse*Math.sin(ucAngle+this.yaw.value);
+        let adjYForce = ucHypotenuse*Math.cos(ucAngle+this.yaw.value);
+        //apply forces to camera
+        this.camera.position.y += (zForce-9.8)/1000 * frameTimeDelta;
+        if (adjXForce > 0)
+            this.camera.position.x += adjYForce/1000 * frameTimeDelta;
+        if (adjYForce > 0)
+            this.camera.position.z += adjXForce/1000 * frameTimeDelta;
+        //apply rotations to camera
+        this.camera.rotation.x = (this.pitch.value + this.camAngleRad);
         this.camera.rotation.y = this.yaw.value;
-        this.camera.rotation.z = this.roll.value;
-
-        console.log(this.camera.rotation);
+        this.camera.rotation.z = -this.roll.value;
     }
-
 }
 
 class Angle {
